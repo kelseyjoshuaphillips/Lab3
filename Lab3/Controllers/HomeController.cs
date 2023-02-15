5c93d2e4fcbe643398befc61b4d9e4bcf233b418
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-02-14 20:39:32 -0500

    submitted project

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            //HttpContext.Session.SetString("Bottles", Request.Form["Bottles"]);
+            ViewBag.Bottles = Request.Form["Bottles"];
+            return View();
+        }
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            if (student.lastName != null && student.firstName != null && student.email != null && student.password != null && student.description != null && student.studentID != null)
+            { return View(student); }
+            return View("Error");
+
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab3/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab3.Models
+{
+    public class Student
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public int? studentID { get; set; }
+        public String email { get; set; }
+        public String password { get; set; }
+        public String description { get; set; }
+    }
+}
